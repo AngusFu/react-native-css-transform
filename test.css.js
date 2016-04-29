f5d1d0b3d3c96f9492d2b27efce9cda0539e709c
付俊 <fj10854@ly.com>
2016-04-29 18:17:49 +0800

    'add'

---
+++ test.css.js
@@ -0,0 +1,21 @@
+
+/**
+ * create by react-native-css-transform
+ * see: https://github.com/AngusFu/react-native-css-transform
+ */
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    "a": {
+        "marginTop": 20,
+        "marginRight": 20,
+        "marginBottom": 20,
+        "marginLeft": 1,
+        "paddingTop": 20,
+        "paddingHorizontal": 20,
+        "paddingBottom": 20
+    }
+});
+
+export default styles;
